a992ed119a4b1a5e02e85f53d8ead77abff14995
micuch11 <micuch1126@gmail.com>
2024-02-26 19:36:03 +0000

    Console App

---
+++ Console App Assignment/Console App Assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Console_App_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            do
+            {
+                Console.WriteLine("Inside the loop");
+                count++;
+            }
+            while (count < 5);
+
+            int counting = 0;
+            while (counting < 3);
+            {
+                Console.WriteLine("Counting numbers");
+                counting++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
